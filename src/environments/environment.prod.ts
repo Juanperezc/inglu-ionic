59c46da4319940785f9f03064a42991fd2533954
Juan Perez <juanl1996@hotmail.com>
2020-07-07 00:02:53 -0400

    prod ionic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   intra_url:  "https://panel.inglu.software",
   googleMapApiKey: 'AIzaSyBSvo0x8v3C6aFWcSi2zooOC9tqGCOqCj4',
-  endpoint: "http://api.inglu.software/api"
+  endpoint: "https://api.inglu.software/api"
   
 };
